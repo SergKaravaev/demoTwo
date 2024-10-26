701c7350e40ecd759d042de98d57b61929bf8a39
skaravaev <sergey.karavaev@artezio.com>
2024-10-26 14:13:24 +0300

    main: add docker-compose.yml

--- run_docker.bat
+++ run_docker.bat
@@ -1,18 +1,1 @@
-@echo off
-SET IMAGE_NAME=demo_two
-SET CONTAINER_NAME=demo_two
-
-FOR /F "tokens=*" %%i IN ('docker ps -q -f "name=%CONTAINER_NAME%"') DO (
-    docker stop %%i
-    docker rm %%i
-)
-
-FOR /F "tokens=*" %%j IN ('docker images -q %IMAGE_NAME%') DO (
-    docker rmi -f %%j
-)
-
-docker build -t %IMAGE_NAME% .
-
-docker run --name %CONTAINER_NAME% -p 8090:8090 -d %IMAGE_NAME%
-
-exit
+docker-compose up --build
